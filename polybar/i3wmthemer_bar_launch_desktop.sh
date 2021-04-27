7746c2435a6df30ebe5f1041636350172f45040e
Martin Jindra <mjindra@derchef.email>
2021-04-27 22:35:54 +0200

    added supposed multi monitor support

---
+++ polybar/i3wmthemer_bar_launch_desktop.sh
@@ -0,0 +1,15 @@
+#!/bin/env sh
+
+pkill polybar
+
+sleep 1;
+
+polybar i3wmthemer_bar &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload i3wmthemer_bar &
+fi
