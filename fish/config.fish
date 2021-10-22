77460c4bd91170988e6e7b2247a7f8eb108cd567
Martin Jindra <mjindra@derchef.email>
2021-10-22 10:24:22 +0200

    update fish config
    Signed-off-by: Martin Jindra <mjindra@derchef.email>

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,8 @@ alias polybarconfig="$EDITOR ~/.config/polybar/config"
 alias ohmyzsh="$EDITOR ~/.oh-my-zsh"
 alias tree="broot -s"
 alias vim="$EDITOR"
-alias ls='exa --color=always --group-directories-first --icons'
-alias ll='exa -al --color=always --group-directories-first --icons'
+alias ls='exa --color=always --group-directories-first --icons --group'
+alias ll='exa -al --color=always --group-directories-first --icons --group'
 alias cat="bat -Pp --color=always"
 alias less="bat --color=always --paging=always"
 alias cp="cp -v"
@@ -21,6 +21,7 @@ alias kdiff="kitty +kitten diff"
 alias remote_file="kitty +kitten remote_file"
 alias hints="kitty +kitten hints"
 alias ssh="kitty +kitten ssh"
+
 alias gitu='git add . && git commit && git push'
 alias gadd="git add"
 alias gc="git commit"
