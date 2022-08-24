28ca13bbb51c2125fb1463de614ad40f188a2d50
Martin Jindra <martin@mjindra.eu>
2022-08-24 14:38:38 +0200

    fish get some vars
    Signed-off-by: Martin Jindra <martin@mjindra.eu>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    ## Variables
+    if type --quiet chromium
+        set -x CHROME_EXECUTABLE (which chromium)
+    end
+
+    ## Paths
+    set -g --path ANDROID_SDK_ROOT /opt/android-sdk
+    fish_add_path -P "$HOME/.local/bin"
+    fish_add_path -P "$HOME/.cargo/bin"
+    fish_add_path -P "$HOME/go/bin"
+    fish_add_path -P "$ANDROID_SDK_ROOT/emulator"
+    fish_add_path -P "$ANDROID_SDK_ROOT/platform-tools"
+    fish_add_path -P "$ANDROID_SDK_ROOT/tools/bin"
+    fish_add_path -P "$ANDROID_SDK_ROOT/tools"
 end
