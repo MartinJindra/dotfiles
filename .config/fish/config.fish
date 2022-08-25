b4cfed70a432efcfd4a725bd777cf9ee9a7b1c9c
Martin Jindra <martin@mjindra.eu>
2022-08-25 21:59:05 +0200

    fish aliases
    Signed-off-by: Martin Jindra <martin@mjindra.eu>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,29 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+    ## Aliases
+    alias ls='exa --color=auto --group-directories-first'
+    alias ll='exa -al --color=auto --group-directories-first --icons --group --git'
+    alias df='duf'
+    alias bat="bat -P --color=auto"
+    alias less="bat --color=auto --paging=always"
+    alias cp="cp -v"
+    alias rm="rm -v"
+    alias mv="mv -v"
+    alias ga="git add"
+    alias gc="git commit"
+    alias gf="git fetch"
+    alias gull="git pull"
+    alias gush="git push"
+    alias gst="git status"
+    alias gl="git log --show-signature --stat --graph"
+    alias giff="git diff --word-diff"
+    alias icat="kitty +kitten icat"
+    alias kdiff="kitty +kitten diff"
+    alias remote_file="kitty +kitten remote_file"
+    alias hints="kitty +kitten hints"
+    alias ksh="kitty +kitten ssh"
+
     ## Variables
     if type --quiet chromium
         set -x CHROME_EXECUTABLE (which chromium)
