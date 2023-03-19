b2ff2a911e990f594326acb6c63bc6cc3a8e31da
Martin Jindra <martin@mjindra.eu>
2023-03-19 14:00:11 +0100

    update fish config
    Signed-off-by: Martin Jindra <martin@mjindra.eu>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,13 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
     ## Aliases
-    alias ls 'exa --color=auto --group-directories-first'
-    alias ll 'exa -al --color=auto --group-directories-first --icons --group --git'
+    #alias ls 'exa --color=auto --group-directories-first'
+    #alias ll 'exa -al --color=auto --group-directories-first --icons --group --git'
     alias df 'duf'
     alias bat 'bat -P --color=auto'
     alias less 'bat --color=auto --paging=always'
     alias cp 'cp -v'
-    alias rm 'rm -v'
+    alias rm 'trash-put'
     alias mv 'mv -v'
     alias ga 'git add'
     alias gc 'git commit'
@@ -27,10 +27,16 @@ if status is-interactive
     ## Variables
     if type --quiet chromium
         set -x CHROME_EXECUTABLE (which chromium)
+    else
+        set -x CHROME_EXECUTABLE "/home/martin/.local/bin/chromium"
     end
 
     ## Paths
-    set -gx --path ANDROID_SDK_ROOT /opt/android-sdk
+    set -gx --path ANDROID_HOME "$HOME/Android/Sdk"
+    set -gx --path NDK_HOME "$HOME/Android/Sdk/ndk/25.2.9519653/"
+    set -gx --path XDG_CONFIG_HOME "$HOME/.config"
+    fish_add_path -P "$HOME/nodejs/bin"
+    fish_add_path -P "$HOME/flutter/bin"
     fish_add_path -P "$HOME/.local/bin"
     fish_add_path -P "$HOME/.cargo/bin"
     fish_add_path -P "$HOME/.yarn/bin"
